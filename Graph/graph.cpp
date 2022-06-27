3d625af63377faa2d7a1dd40536e707edca852d1
Ritika-jain27 <ritikaj088@gmail.com>
2022-06-27 22:06:41 +0530

    DSA

---
+++ Graph/graph.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int n,m;   // n- no. of nodes && m- no. of edges
+cin>>n>>m;
+
+vector<int,vector<int>> adjm(n+1,vector<int>(n+1,0));
+ for(int i=0;i<m;i++)
+ {
+    int x,y;
+    cin>>x>>y;
+ }
+
+return 0;
+}
---
+++ Greedy Algorithm/expedition.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+int t;
+cin>>t;
+
+while(t--)
+{
+    int n;
+    cin>>n;
+    vector<pair<int,int>> a(n);
+    
+
+}
+
+
+return 0;
+}
+
+
+
+
